ea6bb70b149eabdcf5807a60641eb3930858f377
patrick.deschambault <patrick.deschambault@haply.co>
2025-03-29 14:23:03 -0400

    add step_count file to get the encoder count

---
+++ examples/step_count.cpp
@@ -0,0 +1,32 @@
+#include <I2Cdev.h>
+
+//L'encodeur a 100 encoches par tout selon la datasheet
+
+const int encoderPin = 2;  // Pin de l'encodeur
+volatile int encoderCount = 0;  // Compteur d'encoches
+bool isCounting = true;  // Variable pour indiquer si l'on compte un tour complet
+
+void countEncoder();
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(encoderPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(encoderPin), countEncoder, RISING);  // Détection du front montant
+
+}
+
+void loop() {
+  // Si on est en train de compter un tour, on mesure le nombre d'encoches
+  if (isCounting) {
+    // Afficher le nombre d'encoches toutes les secondes
+    Serial.print("Encoches comptées: ");
+    Serial.println(encoderCount);
+  }
+
+}
+
+// Fonction d'interruption pour compter les encoches
+void countEncoder() {
+  encoderCount++;
+}
